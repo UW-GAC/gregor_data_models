3f16490b2ac10621224f3d24393c083430e281fb
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-07-14 12:43:06 -0700

    final updates to v0 of data model
    phenotype rows will be uniquely identified by participant, term
    change table names to include _dna_ to distinguish from rna
    update some column requirements
    fix parsing errors

--- sheets_to_tsv.R
+++ sheets_to_tsv.R
@@ -1,27 +1,59 @@
 library(googlesheets4)
 library(dplyr)
+library(tidyr)
+library(stringr)
 
 url <- "https://docs.google.com/spreadsheets/d/16UKkv3E11ArkSyaTscJrpFNBPdFWfcGr87lNOo6yZyM"
-table_names <- sheet_names(url)[-1:-2] # first two sheets are metadata
-table_names <- setdiff(table_names, "experiment (for future)")
+
+# table metadata
+meta <- read_sheet(url, sheet="Table overview/status")
+
+# remove tables below blank line
+brk <- which(is.na(meta$Table))
+meta <- meta[1:(brk-1),]
+
+meta_tsv <- meta %>%
+    mutate(entity="meta",
+           required=ifelse(tolower(Required) == "yes", TRUE, NA)) %>%
+    select(entity, required, table=Table)
+
+table_names <- meta_tsv$table
 tables <- lapply(table_names, function(x) read_sheet(url, sheet=x, skip=1))
 names(tables) <- table_names
 
 tsv_format <- function(t) {
     tables[[t]] %>%
-        filter(!is.na(Column), !grepl(" ", Column, fixed=TRUE)) %>%
-        mutate(entity="Table", 
-               table=sub(" (prototype)", "", t, fixed=TRUE),
-               type=gsub(" ", "_", `Data Type`, fixed=TRUE),
-               pk=ifelse(grepl("(primary key)", Description, fixed=TRUE), TRUE, NA),
+        filter(!is.na(Column)) %>%
+        mutate(entity="Table",
+               table=t,
+               pk=ifelse(paste0(t, "_id") == Column, TRUE, NA),
+               type=ifelse(`Data Type` == "enumeration", Column, `Data Type`),
                required=ifelse(tolower(Required) == "yes", TRUE, NA)) %>%
         select(entity, table,
                column=Column, type, required,
-               pk, ref=`Cross-table References`,
+               pk, ref=References,
                note=Description)
 }
 
 out <- lapply(table_names, tsv_format) %>%
     bind_rows()
 
-readr::write_tsv(out, file="GREGoR_data_model_v0.tsv")
+# enumerated values
+enum_format <- function(t) {
+    tables[[t]] %>%
+        filter(!is.na(Enumerations)) %>%
+        mutate(entity="enum") %>%
+        select(entity,
+               table=Column,
+               column=Enumerations) %>%
+        separate_rows(column, sep="\n") %>%
+        mutate(column=str_trim(column))
+}
+
+enum_tsv <- lapply(table_names, enum_format) %>%
+    bind_rows %>%
+    distinct()
+
+out <- bind_rows(out, enum_tsv, meta_tsv)
+
+readr::write_tsv(out, file="GREGoR_data_model_v0.tsv", na="")
--- tsv_to_dbml.R
+++ tsv_to_dbml.R
@@ -1,5 +1,8 @@
 #remotes::install_github("UW-GAC/AnvilDataModels")
 
-tsv <- "GREGoR_data_model_v0.tsv"
-dbml <- "GREGoR_data_model_v0.dbml"
-AnvilDataModels::tsv_to_dbml(tsv, dbml)
+prefix <-  "GREGoR_data_model_v0"
+AnvilDataModels::tsv_to_dbml(paste0(prefix, ".tsv"), 
+                             paste0(prefix, ".dbml"))
+
+# check that data model object can be created
+AnvilDataModels::tsv_to_dm(paste0(prefix, ".tsv"))
