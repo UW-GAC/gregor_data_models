e59042bd19106f6569970ed2e11632f69ef67719
Stephanie M. Gogarten <sdmorris@uw.edu>
2023-11-15 14:22:55 -0800

    make change log match json

--- change_log.R
+++ change_log.R
@@ -6,6 +6,13 @@ url <- "https://docs.google.com/spreadsheets/d/1mZlJ9IauaVNiJ6f2Q14pg6rGEpWVrAjM
 
 log <- read_sheet(url, sheet="Change Log") %>%
     filter(!is.na(Version)) %>%
-    mutate(`Change notes` = gsub('"', "'", `Change notes`))
+    mutate(`Change notes` = gsub('"', "'", `Change notes`)) %>%
+    filter(!(Version == "1.3" & Table == "genetic_findings")) %>%
+    mutate(Version = ifelse(Table == "genetic_findings", 1.3, Version)) %>%
+    arrange(Version)
 
 write_tsv(log, file="change_log.tsv")
+
+# markdown table for release notes
+log %>% filter(Version == "1.3") %>%
+    knitr::kable("pipe")
